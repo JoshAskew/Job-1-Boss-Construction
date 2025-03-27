f793a8c1f1b237cb23ffc54b19836d678d280cc6
JoshAskew <asksprit3@gmail.com>
2025-03-27 14:49:11 -0500

    file transfer

---
+++ src/components/Hero.tsx
@@ -0,0 +1,16 @@
+
+import './Hero.css'
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="hero-content">
+        <h1>Welcome to Our Site</h1>
+        <p>Discover amazing things with us</p>
+        <button className="cta-button">Get Started</button>
+      </div>
+    </div>
+  )
+}
+
+export default Hero 
